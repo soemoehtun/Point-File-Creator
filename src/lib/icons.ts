d9e0eefe6034c78745bf7c17f84a8efd3389ec69
Soe Moe Tun <67118878+soemoehtun@users.noreply.github.com>
2026-08-18 22:49:56 +0630

    Delete src directory

--- src/lib/icons.ts
+++
@@ -1,6 +0,0 @@
-import { PALETTE } from "@/types";
-
-export { PALETTE };
-
-export const paletteColor = (i: number) => PALETTE[((i % PALETTE.length) + PALETTE.length) % PALETTE.length];
-export const colorFor = paletteColor;
